8b8d862b0997099debc90b5a8b28332aab338b50
jonivalo <jonivirvalo@gmail.com>
2022-12-06 22:33:00 +0200

    elamyspajafront

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//import { Container, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
+//import { BiSearch } from 'react-icons/bi';
+//import SearchFilter from 'react-filter-search';
+
+export default function Home() {
+  return (
+    <main className="flex-shrink-0 bg-light">
+      
+    </main>
+  )
+}

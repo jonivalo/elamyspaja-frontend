c055cf6c6a7beeb66e3d04d85f60086e7339543f
jesseryy <jesse.ryynala@gmail.com>
2022-12-21 18:53:31 +0200

    vähän lisää

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css';
+import img from '..//images/Logo.png'
 //import { Container, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
 //import { BiSearch } from 'react-icons/bi';
 //import SearchFilter from 'react-filter-search';
@@ -8,7 +9,13 @@ import './Home.css';
 
 export default function Home() {
   return (
-    <div className="homeheader">Rohkeasti valitsemaan tuote</div>
+    <div className="homeheader">
+      <h1>Elämyspaja</h1>
+      <p>Rohkeasti valitsemaan tuote</p>
+      <div>
+        <img src={img} alt="" className='responsive' />
+      </div>
+    </div>
     
   )
 }
